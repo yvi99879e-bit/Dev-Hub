8996c7dc0cd993be4dba6f0805e5122eb18f1530
Vaibhav Verma <yvi99879e@gmail.com>
2026-07-14 06:15:10 +0530

    Import database connection

--- backend/db.js
+++ backend/db.js
@@ -5,15 +5,15 @@ dotenv.config();
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
   if (err) {
-    console.log("Database Connection Failed");
-    console.log(err);
+    console.error(err);
   } else {
     console.log("MySQL Connected Successfully");
   }
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
+import db from "./db.js";
 import express from "express";
+
 import cors from "cors";
 import dotenv from "dotenv";
 
